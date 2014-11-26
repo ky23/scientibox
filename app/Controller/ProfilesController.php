20d40beadfe6350c89686ffec53ef883d46d69bd
Khatal <khata.yacine@gmail.com>
2014-11-26 18:13:22 +0100

    rewrite edit profil

--- app/Controller/ProfilesController.php
+++ app/Controller/ProfilesController.php
@@ -8,34 +8,56 @@ class ProfilesController extends AppController {
 		$this->Auth->allow('edit_profile' , 'warrantly', 'allow', 'deny');
 	}
 
-	public function edit_profile($currentId = null) {
+	public function edit_profile() {
 		$this->loadModel('Applicant');
-		App::uses('NotifEventListener', 'Event');
-		$this->Profile->getEventManager()->attach(new NotifEventListener());
-		if ($currentId == null) {
-			$currentId = $this->Session->read("Applicant.id");
-		}
-		$applicant = $this->Applicant->find('first', array('conditions' => array('Profile.id' => $currentId)));
-		if (!empty($currentId) && !empty($applicant)) {
-			$applicants = $this->Applicant->find('all', array('conditions' => array('company_id' => $applicant['Company']['id'])));
-			$this->set('applicants', $applicants);
-			foreach ($applicants as $applicant) {
-				if ($applicant['Profile']['id'] == $currentId) {
-					$applicant_to_edit = $applicant;
+		// App::uses('NotifEventListener', 'Event');
+		// $this->Profile->getEventManager()->attach(new NotifEventListener());
+		$id = $this->Session->read("Applicant.id");
+		$applicant = $this->Applicant->find('first',
+			array('conditions' => array('Applicant.id' => $id)));
+		if (isset($id) && !empty($applicant)) {
+			if ($this->request->is(array('post', 'put'))) {
+				$token = $this->request->data['tkn'];
+				$applicant = $this->Applicant->find('first',
+					array('conditions' => array('token' => $token)));
+				if (!empty($applicant)) {
+					$applicants = $this->Applicant->find('all', array(
+						'conditions' => array('Applicant.company_id' => $applicant['Company']['id'])));
+					$lenght = count($applicants);
+					$tabId = 0;
+					foreach ($applicants as $key => $value) {
+						if ($applicant['Applicant']['token'] == $value['Applicant']['token']) {
+							if ($key < $lenght)
+								$tabId = $key;
+						}
+					}
+					$data['Profile'] = $this->request->data['Profile'];
+					$data['Profile']['id'] = $applicant['Profile']['id'];
+					$data['Profile']['birthdate'] = date("Y-m-d", strtotime($data['Profile']['birthdate']));
+					if ($this->Profile->save($data, array('validate' => false))) {
+						if ($tabId + 1 < $lenght) {
+							$this->Session->write("tabId", $tabId);
+							return $this->redirect(array(
+								'controller' => 'profiles',
+								'action' => 'edit_profile'));
+						} else {
+							return $this->redirect(array(
+								'controller' => 'profiles',
+								'action' => 'warrantly'));
+						}
+					} else {
+						debug($this->Profile->validationErrors); die();
+					}
 				}
-			}
-			$this->set('current_id', $currentId);
-			$applicant_to_edit['Profile']['birthdate'] = date("d-m-Y", strtotime($applicant_to_edit['Profile']['birthdate']));
-			$this->set('applicant_to_edit', $applicant_to_edit);
-			if ($this->request->is('post', 'put')) {
-				$this->Profile->set($this->request->params['form']);
-				$this->Profile->set("ParentDir", $applicant['Company']['name'] . "_" . $applicant['Company']['id']);
-				$this->request->data['Profile']['id'] = $applicant_to_edit['Profile']['id'];
-				$this->request->data['Profile']['applicant_id'] = $currentId;
-				$this->request->data['Profile']['birthdate'] = date("Y-m-d", strtotime($this->request->data['Profile']['birthdate']));
-				if ($this->Profile->save($this->request->data, false)) {
-					return $this->redirect(array('action' => 'warrantly'));
+			} else {
+				$applicants = $this->Applicant->find('all', array(
+					'conditions' => array('Applicant.company_id' => $applicant['Company']['id'])));
+				foreach ($applicants as &$applicant) {
+					$applicant['Profile']['birthdate'] = date("d-m-Y",
+						strtotime($applicant['Profile']['birthdate']));
 				}
+				//debug($applicants); die();
+				$this->set('applicants', $applicants);
 			}
 		} else {
 			return $this->redirect(array('controller' => 'home', 'action' => 'index'));
